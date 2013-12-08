d790a8b95391505d4f919c69b6d73ba760b9a000
danbar <danbaryak@gmail.com>
2013-12-09 01:24:28 +0200

    blah

--- app.js
+++ app.js
@@ -37,6 +37,6 @@ server.listen(app.get('port'), function(){
 });
 
 io.sockets.on('connection', function(socket) {
-
+   console.log('connected');
 });
 
--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@ var app = angular.module('app', [
     'ngRoute',
     'route-segment',
     'view-segment',
+    'ngAnimate'
 ]);
 
 function NavCtrl($scope, $routeSegment) {
@@ -14,17 +15,32 @@ app.config(function ($routeSegmentProvider, $routeProvider) {
     $routeSegmentProvider
         .when('/', 'first')
         .when('/second', 'second')
+        .when('/second/overview', 'second.overview')
+        .when('/second/info', 'second.info')
 
         .segment('first', {
             templateUrl: 'partials/first.html'
         })
         .segment('second', {
-            templateUrl: 'partials/second.html'
-        })
+            templateUrl: 'partials/second.html',
+            controller: NavCtrl
+        }).
+        within().
+
+        segment('overview', {
+            templateUrl: 'partials/overview.html'}).
+
+        segment('info', {
+            templateUrl: 'partials/info.html'})
+
 
     ;
 
+    socket.on('hello', function (data) {
+        bootbox.alert(data.message);
+    })
 
     $routeProvider.otherwise({redirectTo: '/'});
 
 });
+
